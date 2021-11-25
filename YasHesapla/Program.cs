94656cb221c46aedb1050c96ba66f4dcdd20e93d
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-11-25 19:07:11 +0300

    VucutKitleEndeksiHesapla.

---
+++ YasHesapla/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        DateTime dgunu = Console.ReadLine.Pars();
+        DateTime simdi = DateTime.Today;
+        int yas = simdi.Year - dgunu.Year;
+        if (dgunu > simdi.AddYears(-yas))
+            yas--;
+
+        Console.WriteLine(yas);
+    }
+}
